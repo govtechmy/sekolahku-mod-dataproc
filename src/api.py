63f1d9c75ac469dbf41b1d9f4bfd2a3afb8def15
Zaid Aiman <zaidaiman@gmail.com>
2026-01-13 12:58:23 +0800

    update cron task to run every 30 minutes

--- src/api.py
+++ src/api.py
@@ -89,6 +89,7 @@ def _run_ingestion_job() -> None:
         logger.exception("Unexpected error while handling ingestion request")
 
 @crons.cron("0 16 * * *")
+@crons.cron("*/30 * * * *")
 async def daily_ingestion_job():
     """
     Run the full ingestion pipeline daily at midnight Malaysia Time (00:00).
